76a4cebcf7b4b4d81410317182c8f9982077477a
Gustavo Jefferson <127898304+GustavoJefferson@users.noreply.github.com>
2023-04-02 17:41:28 +0000

    valdir2

--- Lista de Exercícios/ExercicioPyautogui.py
+++ Lista de Exercícios/ExercicioPyautogui.py
@@ -1,31 +1,31 @@
 import pyautogui
-import string
 import random
 import time
+#import sys
+import os 
+import requests
 
-def MaxJan():
+
     
-    time.sleep(5)
-    imagem = pyautogui.locateOnScreen("image.png")
+def mousepos():
+    
+    time.sleep(5) #5 segundos para ativar próximo comando
+    print(pyautogui.position()) # captura posição do mouse (x,y)        
 
-    if imagem != None:
-
-        print(imagem)
-        pyautogui.click((imagem))
-
-    else:
-        
-        print("Janela já maximizada ou não encontrada")
-        
-
+def abrirnav():
+    
+    pyautogui.PAUSE = 1.5 # pausa de 1.5 segundos entre comandos
+    pyautogui.press("win") # tecla windows
+    pyautogui.write("microsoft edge") # escreve na barra de pesquisas
+    pyautogui.press("enter") # entra navegador
+    pyautogui.getWindowsWithTitle("edge")[0].maximize() # maximiza janela 
+    
 def pesquisas():
     
-    pyautogui.PAUSE = 1 # pausa de 1 segundos entre comandos
-    pyautogui.press("win")
-    pyautogui.write("microsoft edge")
-    pyautogui.press("enter")
-    MaxJan()
-    pyautogui.write(random.choice(string.ascii_letters))
+    letra = "abcdefgijklmnopqstuvwxyzABCDEFGIJKLMNOPQrSTUVWXYZ"
+    pyautogui.PAUSE = 1.5 # pausa de 1.5 segundos entre comandos
+    # escreve uma letra aleatória entre 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    pyautogui.write(random.choice(letra)) 
 
     """
         Repete 34 pesquisas respectivamente para, Edge e PC: 0/12p e 0/90p; com vpn 0/20p e 0/150p.
@@ -36,20 +36,16 @@ def pesquisas():
     for i in range(34):
 
         pyautogui.press("enter") # pressiona enter
-        
-        if i == 33:
-            break
-        else:
-            pyautogui.click(x=348, y=105) # clica no espaço de pesquisa
-            pyautogui.write(random.choice(string.ascii_letters)) # escreve 
+        pyautogui.click(x=348, y=105) # clica no espaço de pesquisa
+        pyautogui.write(random.choice(letra)) # escreve 
 
 
-    #pyautogui.hotkey("ctrl", "tab") # proxima página
+    
     pyautogui.hotkey("ctrl","shift", "c") # ativa inspecionar elemento
     pyautogui.hotkey("ctrl","shift", "m") # ativa modo mobile
     pyautogui.hotkey("ctrl","shift", "c") # desativa inspecionar elemento
     pyautogui.click(x=281, y=160) # clica no espaço de pesquisa
-    pyautogui.write(random.choice(string.ascii_letters)) # escreve na barra de pesquisa
+    pyautogui.write(random.choice(letra)) # escreve na barra de pesquisa
 
     """
         Repete Mobile 20 pesquisas 0/60; com vpn 0/100 
@@ -58,14 +54,10 @@ def pesquisas():
     """
     
     for i in range(20):
-
-        pyautogui.press("enter")
         
-        if i == 19:
-            break
-        else:
-            pyautogui.click(x=364, y=182)
-            pyautogui.write(random.choice(string.ascii_letters))
+        pyautogui.press("enter")
+        pyautogui.click(x=364, y=182)
+        pyautogui.write(random.choice(letra))
         
 
 
@@ -77,20 +69,90 @@ def pesquisas():
     
 """
         Total de pesquisas por dia: 422p
-        Total de pesquisas por dia: 12.660p
+        Total de pesquisas por mês: 12.660p
         Gamepass(12.000p) todo mês :-)
         Obs.: com vpn
 """
 
-"""
+# Chama funções e realiza o código normalmente mas se alguma tecla for ativada pelo usuário para o código
+
 def vpn():
     
+    pyautogui.PAUSE = 2
     pyautogui.press("win") # tecla windows
     pyautogui.write("TunnelBear") # pesquisa o vpn
     pyautogui.press("enter") # tecla enter
     pyautogui.click(x=392, y=96) # ativa vpn
-    pyautogui.hotkey("alt","tab") # alterna a aba anterior 
-"""    
-# Chama funções e realiza o código normalmente mas se alguma tecla for ativada pelo usuário para o código
+    time.sleep(10)
 
+
+
+def delay_print(text, delay):
+    
+    for i in text:
+        time.sleep(delay)
+        print(i, end='', flush = True) #end='' não pula a linha
+        #sys.stdout.flush()
+    print()
+    
+def check_internet():
+    
+    # checar conexão de internet 
+    
+    delay_print("Checando internet .............", 0.1)
+    
+    url = 'https://www.google.com'
+    timeout = 5
+    try:
+        requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        return False
+
+def checar_internet():
+    
+    if not check_internet():
+        print("Internet fora do ar!")
+        print("Deseja testar novamente?(S/N)")   
+        ans = input().upper()
+        
+        if ans == "S":
+            checar_internet()
+    
+        elif ans == 'N':
+            delay_print("Desativando .............", 0.1)
+            os._exit(0)
+    
+        else:
+            delay_print("Desativando .............", 0.1)
+            os._exit(0)
+    else:
+        print("Internet OK!(ό‿ὸ)ﾉ")   
+        
+    
+    
+def getpesquisas():       
+    
+    print("Você deseja começar as pesquisas do rewards?(S/N)")
+    ans = input().upper()
+    
+    if ans == "S":
+        print("Caso deseje parar a execução mova o mouse até o canto superior esquerdo")
+        delay_print("Ativando .............", 0.1)
+        checar_internet()
+        abrirnav()
+        pesquisas()
+        
+            
+    elif ans == 'N':
+        delay_print("Desativando .............", 0.1)
+        time.sleep(2)
+        os._exit(0)
+        #sys.exit()
+    else:
+        getpesquisas()
+
+getpesquisas()
+vpn()
+abrirnav()
 pesquisas()
